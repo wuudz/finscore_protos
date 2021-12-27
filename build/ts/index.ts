9e7af53bf4a8cd14b339a59c377b06306d8fef31
Hayden Woods <haydenw@genvis.co>
2021-12-27 16:39:26 +0800

    Add index

---
+++ build/ts/index.ts
@@ -0,0 +1,15 @@
+import { Game, GameConfig, GamePlayer, PlayerOrder } from "./lib/game";
+import { Player } from "./lib/player";
+import { Session } from "./lib/session";
+import { Viewer, ViewerType } from "./lib/viewer";
+
+export default {
+  Game,
+  GameConfig,
+  GamePlayer,
+  PlayerOrder,
+  Player,
+  Session,
+  Viewer,
+  ViewerType,
+};
