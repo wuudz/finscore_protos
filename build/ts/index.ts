2dfa0b9906df9e5a97499025917b7ba1797fc76e
Hayden Woods <haydenw@genvis.co>
2021-12-27 18:34:24 +0800

    Idk

--- build/ts/index.ts
+++ build/ts/index.ts
@@ -1,15 +1,4 @@
-import type { Game, GameConfig, GamePlayer, PlayerOrder } from "./lib/game";
-import type { Player } from "./lib/player";
-import type { Session } from "./lib/session";
-import type { Viewer, ViewerType } from "./lib/viewer";
-
-export {
-  Game,
-  GameConfig,
-  GamePlayer,
-  PlayerOrder,
-  Player,
-  Session,
-  Viewer,
-  ViewerType,
-};
+export type { Game, GameConfig, GamePlayer, PlayerOrder } from "./lib/game";
+export type { Player } from "./lib/player";
+export type { Session } from "./lib/session";
+export type { Viewer, ViewerType } from "./lib/viewer";
