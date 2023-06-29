90ea7e6cfa97a707ffe23b3cb34e83553687f3fe
Hayden Woods <haydenw@genvis.co>
2023-06-29 17:40:34 +0800

    Fix enum exports

--- build/ts/index.ts
+++ build/ts/index.ts
@@ -1,17 +1,15 @@
-export type {
+export {
   Game,
   GameScore,
-  GameStatus,
   GameResolution,
   GameAward,
-  GameAwardType,
   GameConfig,
   GamePlayer,
   GameViewerDataPlayer,
   GameViewerData,
   GameShareData,
-  PlayerOrder,
 } from "./src/game";
+export { GameStatus, PlayerOrder, GameAwardType } from "./src/game";
 export type { UserData, Player } from "./src/user";
 export type { Session } from "./src/session";
 export type { Viewer, ViewerType } from "./src/viewer";
